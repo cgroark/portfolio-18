748ecd623264bf02bdfacaed31841719fd00b229
cgroark <colingroark@gmail.com>
2018-11-14 07:51:54 -0800

    updating about section

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
             <div class="row">
               <div class="col-md-8">
                 <h1>About</h1>
-                <p>I'm a full-stack web developer with experience untangling problems and developing efficient systems and tools in the private and non-profit sectors.</p>
+                <p>I'm a full-stack web developer with experience building websites and untangling problems and developing efficient systems and tools in the private and non-profit sectors.</p>
                 <hr />
                 <p>Working with diverse teams of developers and UX designers, I’ve applied my experience with front-end and back-end technologies to build responsive websites that provide a seamless interaction for users. Prior to engineering, I worked for consulting firms and non-profits where I garnered experience managing programs, teams, budgets and business development processes. When I'm not writing code, you'll find me on my bike or in the Cascades.</p>
               </div>
