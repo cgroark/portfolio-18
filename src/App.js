700dbbd0d3771560e19abac1facbd772fe8ed0b8
cgroark <colingroark@gmail.com>
2018-11-13 21:20:50 -0800

    initial setup and header

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <h1>Colin Groark</h1>
+            <h2>Web Developer</h2>
+          </div>
+          <div>
+          <p>built with <img src={logo} className="App-logo" alt="logo" /></p>
+          </div>
         </header>
+        <main>
+          <article id='about'>
+            <div class="row">
+              <div class="col-md-8">
+                <h1>About</h1>
+                <p>I'm a full-stack web developer with experience untangling problems and developing efficient systems and tools in the private and non-profit sectors.</p>
+                <hr />
+                <p>Working with diverse teams of developers and UX designers, I’ve applied my experience with front-end and back-end technologies to build responsive websites that provide a seamless interaction for users. Prior to engineering, I worked for consulting firms and non-profits where I garnered experience managing programs, teams, budgets and business development processes. When I'm not writing code, you'll find me on my bike or in the Cascades.</p>
+              </div>
+              <div class="col-md-4">
+                <img src="/images/BW-Colin.jpg" alt="headshot of Colin Groark" />
+              </div>
+            </div>
+          </article>
+        </main>
       </div>
     );
   }
