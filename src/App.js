2c5e4db6d57e1133842a3d2ca5f1c4ae09b2e535
cgroark <colingroark@gmail.com>
2018-11-24 15:56:52 -0800

    adding weather project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import WeatherSearch from './Components/WeatherSearch';
 import './App.css';
+require('dotenv').config();
+
 
 class App extends Component {
   render() {
@@ -17,15 +20,23 @@ class App extends Component {
         </header>
         <main>
           <article id='about'>
-            <div class="row">
-              <div class="col-md-8">
+            <div className="row">
+              <div className="col-md-8">
                 <h1>About</h1>
                 <p>I'm a full-stack web developer with experience building websites and untangling problems and developing efficient systems and tools in the private and non-profit sectors.</p>
                 <hr />
                 <p>Working with diverse teams of developers and UX designers, I’ve applied my experience with front-end and back-end technologies to build responsive websites that provide a seamless interaction for users. Prior to engineering, I worked for consulting firms and non-profits where I garnered experience managing programs, teams, budgets and business development processes. When I'm not writing code, you'll find me on my bike or in the Cascades.</p>
               </div>
-              <div class="col-md-4">
+              <div className="col-md-4">
                 <img src="/images/BW-Colin.jpg" alt="headshot of Colin Groark" />
+              </div>
+            </div>
+          </article>
+          <article id='projects'>
+          <h1>Recent Projects</h1>
+            <div className="row">
+              <div className='col-md-6'>
+                <WeatherSearch />
               </div>
             </div>
           </article>
---
+++ src/Components/WeatherSearch.jsx
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import '../App.css';
+import Results from './Results';
+import { FaSearch } from 'react-icons/fa';
+let key = process.env.REACT_APP_WEATHER;
+
+class WeatherSearch extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			city: '',
+			weather: {},
+			forecast: {},
+			search: ''
+		}
+	}
+	handleDelete = (e) => {
+		e.preventDefault()
+		this.setState({
+			weather: {},
+			forecast: {},
+			city: '',
+		})
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			city: e.target.value
+		})
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+		let weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + this.state.city + ',US&APPID=' + key;
+		let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + this.state.city + ',US&APPID=' + key;
+		fetch(weatherUrl)
+			.then(response => {
+				return response.json()
+			})
+			.then(json => {
+				this.setState({
+					weather: json,
+					search: 'yes'
+				})
+				console.log('weatherasdasd', this.state.forecast, this.state.search, this.state.forecast.cnt)
+			})
+
+		fetch(forecastUrl)
+			.then(response => {
+				return response.json()
+			})
+			.then(json => {
+				this.setState({
+					forecast: json
+				})
+			console.log('aforecast', this.state.forecast, this.state.search, this.state.forecast.cnt);
+			console.log(`api key is ${key}`);
+			})
+	}
+	render() {
+		let weather = this.state.weather;
+		let forecast = this.state.forecast;
+		let search = this.state.search;		
+		return(
+			<div className='weather-div'>
+				<form type='input'  onSubmit={this.handleSubmit}>
+					<button type='submit' id='mag-icon'><FaSearch /></button>
+					<input placeholder='City, State' value={this.state.city} onChange={this.handleChange}></input>
+					
+				</form>
+				{forecast && 
+					<button id='delete' onClick={this.handleDelete}>X</button>
+					}
+				<hr></hr>
+				{search && weather.cod === '404' &&
+					<h3>No matching city found</h3>
+				}
+				{forecast.cnt > 0 &&
+				<Results data={this.state.weather} city={this.state.city} forecast={this.state.forecast}/>
+				}
+			</div>
+			)
+	}
+}
+export default WeatherSearch;
